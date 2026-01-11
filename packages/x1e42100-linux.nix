7aee005953738297e40181a4c0fac08f91f40887
Hugo <git@tokoro.fi>
2026-01-11 17:39:55 +0200

    6.17.0 -> 6.18.y

--- packages/x1e42100-linux.nix
+++ packages/x1e42100-linux.nix
@@ -7,11 +7,18 @@
 }:
 
 linuxPackagesFor (buildLinux {
+  #src = fetchFromGitHub {
+  #  owner = "jglathe";
+  #  repo = "linux_ms_dev_kit";
+  #  rev = "jg/ubuntu-qcom-x1e-6.17";
+  #  hash = "sha256-9r+ozCdQhvvS1MRH/j2sSZIMdq65y6uw5Ne2C/pNs8M=";
+  #};
+  #version = "6.17.0";
   src = fetchFromGitHub {
     owner = "jglathe";
     repo = "linux_ms_dev_kit";
-    rev = "jg/ubuntu-qcom-x1e-6.17";
+    rev = "jg/ubuntu-qcom-x1e-6.18.y";
     hash = "sha256-9r+ozCdQhvvS1MRH/j2sSZIMdq65y6uw5Ne2C/pNs8M=";
   };
-  version = "6.17.0";
+  version = "6.18.y";
 })
