bdfb90168204dd316f4a04adb0eb7d3fbea005b4
Hugo Malinen <git@tokoro.fi>
2026-08-20 12:31:45 +0300

    Bump to 7.1.7-jg-1

--- flake.nix
+++ flake.nix
@@ -12,11 +12,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     linux-src = {
-      url = "github:jglathe/linux_ms_dev_kit/jg/ubuntu-qcom-x1e-7.1.7-jg-0";
+      url = "github:jglathe/linux_ms_dev_kit/jg/ubuntu-qcom-x1e-7.1.7-jg-1";
       flake = false;
     };
     slim5x-firmware = {
-      url = "github:Tokor0/linux-firmware-x1p42100-lenovo-ideapad-slim5x/bccb601c19bb2e4e3320a9666114c039836c4b9c";
+      url = "github:Tokor0/linux-firmware-x1p42100-lenovo-ideapad-slim5x";
       flake = false;
     };
   };
